a402136896b7b03fa6c4c8cb027f96c0d67dfb7c
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-05-09 16:55:06 +0530

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
 
 MEDIA_URL = '/images/'
@@ -127,5 +129,5 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'staticfiles')
 ]
-
+django_heroku.settings(locals())
 
